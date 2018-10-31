1951b3e6ac8849edd791c4993c3989642f3432cd
ely-barros <elyb@Ely-MacBook-Pro.local>
2018-10-30 21:52:23 -0300

    Initial Table View

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var coolTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        coolTableView.dataSource = self
+        coolTableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
